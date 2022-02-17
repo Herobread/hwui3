79794142da861051747a6b0d141ca6623f86ca73
Oleksii Nazarenko <alexeybread81@gmail.com>
2022-02-17 21:23:07 +0200

    small timeline improves

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -5,9 +5,9 @@ export default function Table() {
     return <div className={styles.container}>
         <SubjectPanel title={'P.E.'} subtitle={'Gym'} info={'10:20'} isActive={false} />
         <SubjectPanel title={'Ukrainian literature'} subtitle={'121'} info={'11:15'} isActive={false} />
-        <SubjectPanel title={'Geometry'} subtitle={'05:32 left'} info={'121'} isActive={true} />
+        <SubjectPanel title={'Geometry'} subtitle={'121'} info={'05:32'} isActive={true} />
         <SubjectPanel title={'Ukrainian literature'} subtitle={'121'} info={'11:15'} isActive={false} />
-        <SubjectPanel title={'Ukrainian '} subtitle={'121'} info={'11:15'} isActive={false} />
+        <SubjectPanel title={'Algebra '} subtitle={'121'} info={'11:15'} isActive={false} />
         <SubjectPanel title={'Geometry'} subtitle={'121'} info={'11:15'} isActive={false} />
     </div>
 }
