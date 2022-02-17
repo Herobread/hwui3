fe9a57c91482001e5c47883039ed9a35f3572a10
Oleksii Nazarenko <alexeybread81@gmail.com>
2022-02-17 21:03:19 +0200

    improve timeline

--- components/Table/SubjectPanel.js
+++ components/Table/SubjectPanel.js
@@ -6,6 +6,10 @@ export default function SubjectPanel({ title, subtitle, info, isActive }) {
     const circleStyle = isActive ? styles.circle : styles.circle
     const lineStyle = isActive ? styles.line : styles.line
 
+    const headerStyle = isActive ? styles.header + ' ' + styles.headerActive : styles.header
+    const subHeaderStyle = isActive ? styles.subHeader + ' ' + styles.subHeaderActive : styles.subHeader
+    const infoStyle = isActive ? styles.info + ' ' + styles.infoActive : styles.info
+
     return <>
         <div className={styles.timelineContainer}>
             <div className={circleStyle}></div>
@@ -13,9 +17,9 @@ export default function SubjectPanel({ title, subtitle, info, isActive }) {
         </div>
         <div className={styles.wrapper}>
             <section className={containerStyle}>
-                <h4>{title}</h4>
-                <p>{subtitle}</p>
-                <div>{info}</div>
+                <h4 className={headerStyle}>{title}</h4>
+                <p className={subHeaderStyle}>{subtitle}</p>
+                <div className={infoStyle}>{info}</div>
             </section>
         </div>
     </>
--- components/Table/Table.js
+++ components/Table/Table.js
@@ -3,8 +3,11 @@ import styles from './Table.module.css'
 
 export default function Table() {
     return <div className={styles.container}>
-        <SubjectPanel title={'Geometry'} subtitle={'05:32 left'} info={'121'} isActive={true} />
         <SubjectPanel title={'P.E.'} subtitle={'Gym'} info={'10:20'} isActive={false} />
         <SubjectPanel title={'Ukrainian literature'} subtitle={'121'} info={'11:15'} isActive={false} />
+        <SubjectPanel title={'Geometry'} subtitle={'05:32 left'} info={'121'} isActive={true} />
+        <SubjectPanel title={'Ukrainian literature'} subtitle={'121'} info={'11:15'} isActive={false} />
+        <SubjectPanel title={'Ukrainian '} subtitle={'121'} info={'11:15'} isActive={false} />
+        <SubjectPanel title={'Geometry'} subtitle={'121'} info={'11:15'} isActive={false} />
     </div>
 }
